28ae230fbb86547bfad7945bb04ae6fe27c694a9
Reynaldo <reynaldo.marinho@globalsys.com.br>
2019-11-16 00:01:45 -0300

    uncomment tests

--- e2e/LoginScreen.spec.js
+++ e2e/LoginScreen.spec.js
@@ -7,48 +7,48 @@ describe('Login Screen', () => {
     await expect(element(by.id('emailField'))).toBeVisible()
   })
 
-  // it('should have a password field', async () => {
-  //   await expect(element(by.id('passwordField'))).toBeVisible()
-  // })
+  it('should have a password field', async () => {
+    await expect(element(by.id('passwordField'))).toBeVisible()
+  })
 
-  // it('should have a login button', async () => {
-  //   await expect(element(by.id('loginButton'))).toBeVisible()
-  // })
+  it('should have a login button', async () => {
+    await expect(element(by.id('loginButton'))).toBeVisible()
+  })
 
-  // it('should have a register button', async () => {
-  //   await expect(element(by.id('registerButton'))).toBeVisible()
-  // })
+  it('should have a register button', async () => {
+    await expect(element(by.id('registerButton'))).toBeVisible()
+  })
 
-  // it('should require email and password', async () => {
-  //   // await element(by.id('emailField')).typeText('notvalidemail@email')
-  //   await element(by.id('loginButton')).tap()
-  //   await expect(element(by.id('emailErrorText'))).toBeVisible()
-  //   await expect(element(by.id('passwordErrorText'))).toBeVisible()
-  // })
+  it('should require email and password', async () => {
+    // await element(by.id('emailField')).typeText('notvalidemail@email')
+    await element(by.id('loginButton')).tap()
+    await expect(element(by.id('emailErrorText'))).toBeVisible()
+    await expect(element(by.id('passwordErrorText'))).toBeVisible()
+  })
 
-  // it('should require only password when email field is valid', async () => {
-  //   await element(by.id('emailField')).typeText('test@email.com')
-  //   await element(by.id('loginButton')).tap()
-  //   await expect(element(by.id('emailErrorText'))).toBeNotVisible()
-  //   await expect(element(by.id('passwordErrorText'))).toBeVisible()
-  // })
+  it('should require only password when email field is valid', async () => {
+    await element(by.id('emailField')).typeText('test@email.com')
+    await element(by.id('loginButton')).tap()
+    await expect(element(by.id('emailErrorText'))).toBeNotVisible()
+    await expect(element(by.id('passwordErrorText'))).toBeVisible()
+  })
 
-  // it('should require only email when password field is valid', async () => {
-  //   await element(by.id('emailField')).clearText()
-  //   await element(by.id('passwordField')).typeText('123qwe')
-  //   await element(by.id('loginButton')).tap()
-  //   await expect(element(by.id('emailErrorText'))).toBeVisible()
-  //   await expect(element(by.id('passwordErrorText'))).toBeNotVisible()
-  // })
+  it('should require only email when password field is valid', async () => {
+    await element(by.id('emailField')).clearText()
+    await element(by.id('passwordField')).typeText('123qwe')
+    await element(by.id('loginButton')).tap()
+    await expect(element(by.id('emailErrorText'))).toBeVisible()
+    await expect(element(by.id('passwordErrorText'))).toBeNotVisible()
+  })
 
-  // it('should show message when login fails', async () => {
-  //   await element(by.id('passwordField')).clearText()
-  //   await element(by.id('emailField')).typeText(
-  //     'usuarionaocadastrado@email.com'
-  //   )
-  //   await element(by.id('passwordField')).typeText('123456')
-  //   await element(by.id('loginButton')).tap()
-  //   await expect(element(by.id('modalLoginError'))).toBeVisible()
-  //   await element(by.id('buttonOkModalLoginError')).tap()
-  // })
+  it('should show message when login fails', async () => {
+    await element(by.id('passwordField')).clearText()
+    await element(by.id('emailField')).typeText(
+      'usuarionaocadastrado@email.com'
+    )
+    await element(by.id('passwordField')).typeText('123456')
+    await element(by.id('loginButton')).tap()
+    await expect(element(by.id('modalLoginError'))).toBeVisible()
+    await element(by.id('buttonOkModalLoginError')).tap()
+  })
 })
