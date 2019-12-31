1cbb6fb6416825148054697d87716679fa5fb5fb
ReynaldoMarinho <reynaldommaciel@gmail.com>
2019-12-31 09:42:04 -0300

    remove consoles

--- src/utils/masks/telefone.js
+++ src/utils/masks/telefone.js
@@ -2,11 +2,9 @@ export default telefone => {
   // Remove letters dots and hyphens
   if (telefone) {
     let numbers = telefone.toString().replace(/[A-Z]|[a-z]|\.|\-|\(|\)\ /g, '')
-    console.log(numbers)
     const part1 = numbers.substr(0, 2)
     const part2 = numbers.substr(2, 5)
     const part3 = numbers.substr(7, 4)
-    console.log(part3)
     let newTelefone = `(${part1}) `
     if (part2) {
       newTelefone += part2
